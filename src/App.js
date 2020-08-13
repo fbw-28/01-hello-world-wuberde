409d4d3c7fbaf5b05deb4a2e08bca10b1a553673
wuberde <wuberde@gmail.com>
2020-08-13 12:39:38 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import "./App.scss"
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <h1> Hello World </h1>{' '}
     </div>
+     /* <React.Fragment>
+    (instead of using div, just use the fragment, inside can add h1, h2 multiple elements)
+    </React.Fragment>
+     */   
   );
 }
 
